d8c7df2256bc6bd4e3428118cfc30d5b6d99e16e
Nikita <nickitque.39@gmail.com>
2024-02-13 09:58:27 +0100

    Added password changes template

--- socialproject/users/urls.py
+++ socialproject/users/urls.py
@@ -3,8 +3,13 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_view
 
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.user_login, name='login'),
     path('logout/', auth_view.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('password_change/', auth_view.PasswordChangeView.as_view(template_name='password_change_form.html'),
+         name='password_change'),
+    path('password_change_done/', auth_view.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
+         name='password_change_done'),
 ]
