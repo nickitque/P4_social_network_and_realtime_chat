d4acbf8f281c9c20113467a5bcdaeb13a5f9fe21
Nikita <nickitque.39@gmail.com>
2024-02-13 09:06:17 +0100

    Created user authentication

--- socialproject/users/views.py
+++ socialproject/users/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
-# Create your views here.
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(request, username=data['username'], password=data['password'])
+
+            if user is not None:
+                login(request, user)
+                return HttpResponse("User authenticated")
+            else:
+                return HttpResponse("Invalid creds ")
+
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
