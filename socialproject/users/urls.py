e52bcab2899a12e7e217077e7cbecf3094604e9c
Nikita <nickitque.39@gmail.com>
2024-02-13 09:31:49 +0100

    Created templates including base template

--- socialproject/users/urls.py
+++ socialproject/users/urls.py
@@ -1,6 +1,9 @@
+from re import template
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_view
 
 urlpatterns = [
     path('login/', views.user_login, name='login'),
+    path('logout/', auth_view.LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
